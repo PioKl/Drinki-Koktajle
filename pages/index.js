5cb8c99671c2fefbf9e5f9ae3837582bd82b1843
PioKl <kosiu342@gmail.com>
2021-06-01 17:45:39 +0200

    Add comment about getStaticProps and early label functionality (comment)

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
+import React, { useState } from 'react';
 import { createClient } from 'contentful'; //połączenie z contenful
 import DrinkCard from '../components/DrinkCard';
 
+
+//getStaticProps pobranie danych zanim komponent się wyrenderuje, ta funkcja działa gdy aplikacja jest "budowana (build time)", przed wyrenderowaniem sie komponentu, nie działa ona w przeglądarce, tylko podczas buildu, w tej funkcji nie można zamieścić żadnego kodu, który ma działać w przeglądarce, podczasz użytkowania np. zmienne zwiazane useState. Funkcja getStaticProps uruchamia się w czasie budowania strony (uruchamia sie jako pierwszy, przed wyrenderowaniem komponentu) i przekazuje parametry do podstrony. Kod zawarty tutaj działa po stronie serwera, kod umieszczony w getStaticProps nie jest widoczny dla klienta (client)
 export async function getStaticProps() {
 
   const client = createClient({
@@ -13,17 +16,25 @@ export async function getStaticProps() {
   return {
     props: {
       drinks: response.items,
+      //revalidate: 1//jak często next js ma sprawdzać, czy nastąpiła aktualizacja zawartości, tu np. oznacza, że ma sprawdzać co sekundę
     }
   }
 }
 
 export default function Drinks({ drinks }) {
 
+  const [value, setValue] = useState("");
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+    setValue(e.target.value);
+  }
+
   console.log(drinks)
 
   //wyszukiwanie drinka po nazwie
   const searchDrink = drinks.filter(drink => {
-    return drink.fields.drinkName.includes('Mojito')
+    return drink.fields.drinkName.toLocaleLowerCase().includes(value.toLocaleLowerCase())
   })
   console.log(searchDrink)
 
@@ -46,7 +57,17 @@ export default function Drinks({ drinks }) {
 
   return (
     <div className="drinksList">
-      {drinks.map(drink => (
+      <input type="text" value={value} onChange={handleChange} />
+      {/*       <label>
+        Wybierz swój ulubiony smak:
+        <select value={value} onChange={handleChange}>
+          <option value=""></option>
+          {searchDrink.map(drink => (
+            <option value={drink.fields.drinkName}>{drink.fields.drinkName}</option>
+          ))}
+        </select>
+      </label> */}
+      {searchDrink.map(drink => (
         <DrinkCard key={drink.sys.id} drink={drink} />
       ))}
     </div>
